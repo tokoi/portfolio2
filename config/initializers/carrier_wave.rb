d6d1fba42502c2a87e7acf85755c013565751ab7
Tokoi Kazunori <trustsport12quruli@yahoo.co.jp>
2017-11-15 13:52:06 +0000

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['AKIAI5E4GGBDYNX27G7Q'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
